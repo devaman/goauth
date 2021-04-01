dcafd6ba162dd161f95f8f8a4eb8ec58610da7c5
Amit Chambial <chambialamit@gmail.com>
2021-04-01 21:05:00 +0530

    push notif

--- server.js
+++ server.js
@@ -4,17 +4,26 @@ const express = require("express"),
     bodyParser = require("body-parser"),
     morgan = require("morgan"),
     app = express(),
-    passport = require("passport")
+    passport = require("passport"),
     pass = require('./passport'),
     cookieParser = require('cookie-parser'),
-    cookieSession = require('cookie-session');
+    cookieSession = require('cookie-session'),
+    webpush = require('web-push'),
+    serve = require('express-static');
 
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+
+// Replace with your email
+webpush.setVapidDetails('mailto:chambialamit@gmail.com', publicVapidKey, privateVapidKey);
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cookieSession({
     name: 'session',
     keys: ['goauth'],
-    maxAge:7*24*60*60*1000,
-  }))
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+}))
 app.use(passport.initialize());
 app.use(passport.session());
 app.set('view engine', 'ejs');
@@ -23,9 +32,17 @@ app.set('view engine', 'ejs');
 app.get("/auth/google",
     passport.authenticate(
         "google", {
-            scope: ["profile", "email"]
-        }));
-
+        scope: ["profile", "email"]
+    }));
+app.post('/btn', (req, res) => {
+    const subscription = req.body;
+    console.log("btn", subscription);
+    const payload = JSON.stringify({ title: 'test' });
+    webpush.sendNotification(subscription, payload).catch(error => {
+        console.error(error.stack);
+    });
+    res.json({success:true})
+})
 // #2
 app.get("/auth/google/callback",
     passport.authenticate("google", {
@@ -34,14 +51,19 @@ app.get("/auth/google/callback",
         scope: ["profile", "email"]
     })
 );
+//subscribe event
+app.post('/subscribe', (req, res) => {
+    const subscription = req.body;
+    res.status(201).json({});
 
-app.get("/",isLoggedIn, function(req, res) {
-    res.render('profile1',{user:req.user,picture:req.user._json.picture.split("=")[0]})
 });
-app.get("/profile2",isLoggedIn, function(req, res) {
-    res.render('profile',{user:req.user,picture:req.user._json.picture.split("=")[0]})
+app.get("/", isLoggedIn, function (req, res) {
+    res.render('profile1', { user: req.user, picture: req.user._json.picture.split("=")[0] })
 });
-app.get('/logout',function(req,res){
+app.get("/profile2", isLoggedIn, function (req, res) {
+    res.render('profile', { user: req.user, picture: req.user._json.picture.split("=")[0] })
+});
+app.get('/logout', function (req, res) {
     req.session = null;
     res.redirect('/signin')
 })
@@ -54,14 +76,15 @@ function isLoggedIn(req, res, next) {
     res.redirect("/signin");
 }
 
-app.get("/signin", function(req, res) {
+app.get("/signin", function (req, res) {
     console.log("req.user: " + req.user);
     res.render('login')
 });
 
-app.get("/google_callback_fail", function(req, res) {
+app.get("/google_callback_fail", function (req, res) {
     res.json("the callback after google DID NOT authenticate the user");
 });
+app.use(serve(__dirname + '/public'));
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
